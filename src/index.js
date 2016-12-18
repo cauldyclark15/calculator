bff257ac17f72723c3e59660b4b3ffca6bee1022
Joselie Castaneda <cauldyclark15@jc.local>
2016-12-19 05:00:49 +0700

    Add logging function

--- src/index.js
+++ src/index.js
@@ -8,6 +8,21 @@ import './index.css';
 
 const store = createStore(calculatorReducer);
 
+const addLoggingToDispatch = (store) => {
+  const rawDispatch = store.dispatch;
+  return (action) => {
+    console.group(action.type);
+    console.log('%c prev state', 'color: gray', store.getState());
+    console.log('%c action', 'color: blue', action);
+    const returnValue = rawDispatch(action);
+    console.log('c% next state', 'color: green', store.getState());
+    console.groupEnd(action.type);
+    return returnValue;
+  };
+};
+
+store.dispatch = addLoggingToDispatch(store);
+
 render(
   <Provider store={store}>
     <CalcuForm />
