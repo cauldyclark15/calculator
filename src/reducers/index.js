c8467a87735261ee8ff536b2e63ac0366e92ab4f
Joselie Castaneda <cauldyclark15@jc.local>
2016-12-20 23:56:35 +0700

    Fix initialstate

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,13 +3,10 @@ import {combineReducers} from 'redux';
 const initialState = {
     history: '',
     answer: '0',
-    firstNum: 0,
-    secondNum: 0,
+    previous: 0,
     operation: '',
     toOperation: true,
-    toAnswer: false,
     toDecimal: true,
-    toSecondNum: false,
     equalPressed: false,
 };
 
@@ -49,46 +46,42 @@ const CalcuReducer = (state = initialState, action) => {
             if (state.equalPressed) {
                 return Object.assign({}, state, {
                     answer: action.digit,
-                    firstNum: getNum(0, action.digit),
+                    previous: getNum(0, action.digit),
                     equalPressed: !state.equalPressed
                 });
             }
             return Object.assign({}, state, {
-                answer: state.firstNum === 0 ? action.digit : displayAnswer(state.answer, action.digit),
+                answer: !state.toOperation ? action.digit : displayAnswer(state.answer, action.digit),
+                toOperation: true
             });
         case 'INPUT_OPERATION':
             if (!state.toOperation) {
                 return state;
             }
-            if (state.toAnswer) {
-                if (state.secondNum === 0 && state.operation === '÷') {
-                    return Object.assign({}, initialState, {
-                        answer: 'Error'
-                    });
-                }
+            if (!!state.answer && !!state.previous) {
                 return Object.assign({}, state, {
                     history: state.history + ' ' + state.answer + ' ' + action.operation,
-                    answer: getAnswer(state.firstNum, parseFloat(state.answer), state.operation).toString(),
+                    answer: getAnswer(state.previous, parseFloat(state.answer), state.operation).toString(),
                     toOperation: state.toOperation ? !state.toOperation : state.toOperation,
-                    toAnswer: !state.toAnswer,
-                    firstNum: getAnswer(state.firstNum, parseFloat(state.answer), state.operation),
-                    operation: action.operation
+                    previous: getAnswer(state.previous, parseFloat(state.answer), state.operation),
+                    operation: action.operation,
+                    equalPressed: false,
                 });
             }
             return Object.assign({}, state, {
-                history: state.answer + ' ' + action.operation,
+                history: parseFloat(state.answer).toString() + ' ' + action.operation,
                 toOperation: state.toOperation ? !state.toOperation : state.toOperation,
                 operation: action.operation,
-                firstNum: parseFloat(state.answer),
+                previous: parseFloat(state.answer),
+                equalPressed: false,
             });
         case 'GET_ANSWER':
             return Object.assign({}, state, {
                 history: '',
-                answer: getAnswer(state.firstNum, state.secondNum, state.operation).toString(),
-                firstNum: getAnswer(state.firstNum, state.secondNum, state.operation),
+                answer: getAnswer(state.previous, parseFloat(state.answer), state.operation).toString(),
+                previous: 0,
                 operation: '',
                 toOperation: state.toOperation ? state.toOperation : !state.toOperation,
-                toAnswer: false,
                 equalPressed: state.equalPressed ? state.equalPressed : !state.equalPressed,
             });
         case 'PUT_DECIMAL_POINT':
@@ -96,7 +89,7 @@ const CalcuReducer = (state = initialState, action) => {
                 return state;
             }
             return Object.assign({}, state, {
-                answer: state.firstNum.toString() + action.dot,
+                answer: state.answer + action.dot,
                 toDecimal: !state.toDecimal,
             });
         default:
