cbf4da0ddc03e65f720481d0b04ff40da71b4c73
Joselie Castaneda <cauldyclark15@jc.local>
2016-12-21 14:58:20 +0700

    Install react-bootstrap

--- src/components/NumButtonCell.js
+++ src/components/NumButtonCell.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import {inputDigit} from '../actions/action';
+//import {Button} from 'react-bootstrap';
 
 let NumButtonCell = ({dispatch, children}) => (
     <td>
-        <button onClick={() => {
+        <button className="btn btn-primary btn-sm" onClick={() => {
             dispatch(inputDigit(children));
         }}>
             {children}
